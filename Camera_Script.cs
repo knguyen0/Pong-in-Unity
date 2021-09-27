0a2fdd354c082a93fd98f0806ad8675ef033e9a3
knguyen0 <39286084+knguyen0@users.noreply.github.com>
2021-09-27 02:04:49 -0400

    Add files via upload

--- Camera_Script.cs
+++ Camera_Script.cs
@@ -4,22 +4,38 @@ using UnityEngine;
 
 public class Camera_Script : MonoBehaviour
 {
-    Color bgcolor; Color current; public Camera camera1;
-    // Start is called before the first frame update
-    void Start()
-    {
-        InvokeRepeating("Change_color", 0.0f, 5.0f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    void Change_color()
-    {
-        current = new Color(Random.value, Random.value, Random.value);
-        bgcolor = new Color(Random.value, Random.value, Random.value);
-        camera1.backgroundColor = Color.Lerp(current, bgcolor, 5.0f);
-    }
+    public Color color1;
+ public Color color2;
+ 
+ float time;
+ float time2;
+ float transition;
+ 
+ int firstColor = 0;
+ void Update()
+ {
+     if (firstColor == 0)
+     {
+         color1 = Random.ColorHSV(Random.value, Random.value);
+         color2 = Random.ColorHSV(Random.value, Random.value);
+ 
+         firstColor = 1;
+     }
+ 
+     Camera.main.backgroundColor = Color.Lerp(color2, color1, transition);
+ 
+     time += Time.deltaTime;
+     time2 += Time.deltaTime;
+     transition = time2 / 5;
+ 
+     if (time > 5)
+     {
+         color2 = color1;
+         color1 = Random.ColorHSV(Random.value, Random.value);
+ 
+         time = 0;
+         time2 = 0;
+         transition = 0;
+     }
+ }
 }
